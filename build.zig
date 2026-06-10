250aa681d634bd7fce552b5d3cb9b832dab60b62
horenso <jannis.adamek@gmail.com>
2026-06-11 00:22:31 +0200

    Update to latest Zig 0.17.0-dev.813+2153f8143

--- build.zig
+++ build.zig
@@ -23,14 +23,12 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the app");
 
     run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    run_cmd.addPassthruArgs();
 
-    const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
 
